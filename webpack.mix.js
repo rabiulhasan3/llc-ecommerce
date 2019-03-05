f41b7dd87846f7f4286604dcaf5e14cc54585562
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-05 12:58:15 +0600

    solve js & css file linkup with webmix

--- webpack.mix.js
+++ webpack.mix.js
@@ -14,12 +14,12 @@ const mix = require('laravel-mix');
 // mix.js('resources/js/app.js', 'public/js')
 //    .sass('resources/sass/app.scss', 'public/css');
 
-mix.sass('resources/sass/app.scss', 'public/css')
+mix.js('resources/js/app.js', 'public/js')
+   .sass('resources/sass/app.scss', 'public/css')
    .styles([
       'resources/css/custom.css'
    ],'public/css/all.css')
    .scripts([
-      'resources/js/app.js',
       'resources/js/custom.js'
    ],'public/js/all.js')
    .version();
