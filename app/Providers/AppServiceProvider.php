43a8783304aeb893faf8670316f1a550d3d0342e
rabiulhasan3 <rabiul.fci@gmail.com>
2019-03-06 10:46:01 +0600

    category list added

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Support\Facades\Schema;
+use App\Models\Category;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,9 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
+
+        $categories = Category::select(['name','slug'])->where('category_id', null)->get();
+        view()->share('categories',$categories);
     }
 
     /**
